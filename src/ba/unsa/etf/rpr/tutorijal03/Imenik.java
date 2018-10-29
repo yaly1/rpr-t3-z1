6a83a0f2adb178e43a70267c44170bb8303a42db
yaly1 <yaly1@etf.unsa.ba>
2018-10-29 23:40:24 +0100

    Class "Imenik" added

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import static ba.unsa.etf.rpr.tutorijal03.FiksniBroj.Grad;
+
+import java.util.HashMap;
+import java.util.Set;
+
 public class Imenik {
+    //HashMap<>;
+    public void dodaj(String ime, TelefonskiBroj broj) {
+
+    }
+
+    public String dajBroj(String ime) {
+        return null;
+    }
+
+    public String dajIme(TelefonskiBroj broj) {
+        return null;
+    }
+
+    public String naSlovo(char s) {
+        return null;
+    }
+
+    public Set<String> izGrada(Grad g) {
+        return null;
+    }
+
+    public Set<TelefonskiBroj> izGradaBrojevi(Grad g) {
+        return null;
+    }
 }
